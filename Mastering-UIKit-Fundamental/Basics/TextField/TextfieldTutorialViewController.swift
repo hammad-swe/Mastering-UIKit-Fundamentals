c60fe4b38fadd903a5dc4fb0a3e992c037960952
Hammad Ali <hammadali78056@gmail.com>
2025-11-21 10:36:54 +0500

    Add delegate and editing handlers to text field
    Set the text field's delegate and add an editingChanged target to handle text changes. Implement UITextFieldDelegate methods to log debug messages for return key, begin editing, and end editing events.

--- Mastering-UIKit-Fundamental/Basics/TextField/TextfieldTutorialViewController.swift
+++ Mastering-UIKit-Fundamental/Basics/TextField/TextfieldTutorialViewController.swift
@@ -14,21 +14,39 @@ class TextfieldTutorialViewController: UIViewController {
         tf.placeholder = "Search...."
         tf.keyboardType = .emailAddress
         tf.borderStyle = .roundedRect
+        tf.returnKeyType = .search
         tf.font = UIFont.systemFont(ofSize: 14)
+        tf.returnKeyType = .search
         return tf
     }()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
+        textfield.delegate = self
+        textfield.addTarget(self, action: #selector(onEditingChanged), for: .editingChanged)
+        
        view.addSubview(textfield)
        textfield.translatesAutoresizingMaskIntoConstraints = false
         textfield.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         textfield.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         textfield.widthAnchor.constraint(equalToConstant: view.frame.width - 64).isActive = true
     }
-    
-
+    @objc func onEditingChanged(_ sender: UITextField){
+        print("Debug: Text field text \(sender.text)")
+    }
+}
+extension TextfieldTutorialViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("Debug: Return Key Tapped... ")
+       return true
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField){
+        print("Debug: Did Begin Edithing... ")
+    }
+    func textFieldDidEndEditing(_ textField: UITextField){
+        print("Debug: Did Begin ending... ")
+    }
 }
 #Preview {
     TextfieldTutorialViewController()
